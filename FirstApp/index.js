27ed7e96005ca13a18721b8c41cbe1fde1a394cf
prostfk@gmail.com <prostfk@gmail.com>
2018-05-08 19:36:53 +0300

    add html resolving

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,8 +1,12 @@
 // first http project on node.js
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(function(request,response) {
-	response.writeHead(200, {'Content-Type':'text/plan; charset=utf-8'});
-	response.end('Hello, http');
+	response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+	var content = fs.createReadStream(__dirname + "/index.html", "utf8");
+	content.pipe(response);
+
 });
+
 server.listen(3001, '127.0.0.1');
 console.log('port 3000 is using now');
